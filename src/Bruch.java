1e0c2a11d587df7ce477a960cefaf6d7048892c7
MarioZweidick <mario.zweidick2@gmail.com>
2021-10-23 11:52:01 +0200

    implemented multiplicate method in two different ways and reworked code

--- src/Bruch.java
+++ src/Bruch.java
@@ -2,7 +2,8 @@ public class Bruch {
     private int numerator; // Zähler
     private int denominator; // Nenner
 
-    public Bruch(int numerator, int denominator) {
+    public Bruch(int numerator, int denominator)
+    {
         // TODO: Setzen Sie die Attribute entsprechend
         // set variables in constructor
         this.numerator = numerator;
@@ -19,19 +20,22 @@ public class Bruch {
         return denominator;
     }
 
-    public double toDecimal() {
+    public double toDecimal()
+    {
         // TODO: Die Methode soll den Dezimalwert zum Bruch retournieren
         // Achtung: Ganzzahldivision!
         //Return double value
         return ((double) numerator)/denominator;
     }
 
-    public String print() {
+    public String print()
+    {
         // TODO: Die Methode soll den Bruch als Text retournieren z.B.: "5 / 7"
         return numerator + "/" + denominator;
     }
 
-    public Bruch multiplicate(Bruch b2) {
+    public Bruch multiplicate(Bruch b2)
+    {
         /* TODO:
         *  Erstellen Sie für das Ergebnis eine neue Bruch-Variable
         *   => Übergeben Sie im Konstruktur für numerator den
@@ -40,13 +44,15 @@ public class Bruch {
         *  Retournieren Sie ihre neue Bruch-Variable
         * */
 
-
-        return null;
+        //return new Bruch, after multiplying it with the Bruch variable of the parameter list
+        return new Bruch(numerator * b2.getNumerator(), denominator * b2.getDenominator());
     }
 
-    public Bruch multiplicate(Bruch b2,  Bruch b3) {
+    public Bruch multiplicate(Bruch b2,  Bruch b3)
+    {
         // TODO: Multiplizieren Sie den eigenen Bruch mit b2 und b3
-
-        return null;
+        //return new Bruch, after multiplying it with the Bruch variables of the parameter list
+        return new Bruch(numerator * b2.getNumerator() * b3.getNumerator(),
+                       denominator * b2.getDenominator() * b3.getDenominator());
     }
 }
